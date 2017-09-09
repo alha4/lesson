4a5af2aa8b60e2280ee8761ad8b6b751c095dbbe
Sam <buy-too@mail.ru>
2017-09-09 19:23:58 +0500

    operate isset

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+const APIKEY = '12121';
+
+function debug(?int $id) : array {
+	
+	
+	return [1,23,4,$id];
+}
+
+function Users(array $data) : iterable {
+	
+	foreach($data as $item) {
+		 yield $item; 
+	}
+}
+
+$command = $_GET['cmd'] ?? 'not defined';
+
+try {
+	
+   $date = Users(debug(333));
+   //echo APIKEY;
+   
+   $res = $date->current();
+   $res = $date->next();
+   $res2 = $date->current();
+   print_r($res2);
+   
+} catch(Exception $e) {
+	
+	echo $e->getMessage();
+}
+?>
+
