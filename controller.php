9ef1d5e88d7abe97de4274608203fe78ecc3920e
Sam <buy-too@mail.ru>
2017-09-09 22:17:48 +0500

    consnt echo

--- controller.php
+++ controller.php
@@ -1,4 +1,4 @@
-<?
+<?php
   class FrontController {
 	  
 	 private const SECRET_KEY = 'ash4h5hdsnshagwsf5452';
@@ -9,6 +9,9 @@
 		 echo self::SECRET_KEY;
 	 }
   }
-  
+ try {
   $contrll = new FrontController();
+ } catch(Exception $e) {
+	echo $e->getMessage(); 
+ }
 ?>
