0d16363e776b6f775bd9fc177ea8474cf790a3b0
Sam <buy-too@mail.ru>
2017-09-09 21:49:47 +0500

    remote pull

--- index.php
+++ index.php
@@ -21,11 +21,15 @@ $command = $_GET['cmd'] ?? 'not defined';
 try {
 	
    $date = Users(debug(333));
-   echo APIKEY;
+   //echo APIKEY;
    
    $res = $date->current();
    $res = $date->next();
    $res2 = $date->current();
+   print_r($res2);
+   
+
+   echo $command;
    
 } catch(Exception $e) {
 	
